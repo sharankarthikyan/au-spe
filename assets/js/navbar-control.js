d4c03b12040da6ea7c6dc9f0c376c6d407d69684
gokulkrishna-k <gokulkrishna.k.804@gmail.com>
2021-08-25 21:49:23 +0530

    added images, fixed nav

--- assets/js/navbar-control.js
+++ assets/js/navbar-control.js
@@ -1,13 +1,19 @@
-const body = document.querySelector("body");
+const body = document.querySelector('body');
 
 const canScroll = function (e) {
-  const check = document.querySelector("#check");
-  console.log(check);
-  if (!check.checked) {
+  const check = document.querySelector('#check');
+
+  if (check.checked) {
     // Disable scroll
-    body.style.overflow = "hidden";
+    body.style.overflow = 'auto';
   } else {
     // Enable scroll
-    body.style.overflow = "auto";
+    body.style.overflow = 'hidden';
   }
+};
+
+const hideNav = function (e) {
+  const check = document.querySelector('#check');
+  check.checked = true;
+  body.style.overflow = 'auto';
 };
